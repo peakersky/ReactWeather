3c6d482006ddd6a2e4d7f72e63e77713581d7697
David Lee <peakersky@gmail.com>
2016-12-22 23:07:12 +0900

    Add grid system

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,13 +1,4 @@
 var React = require('react');
-//
-// var WeatherMessage = React.createClass({
-//   render: function(location, temp) {
-//     var {temp, location} = this.props;
-//       return (
-//         <p>It is {temp} in {location}</p>
-//     )
-//   }
-// });
 
 var WeatherMessage = ({temp, location})=> {
   return (
